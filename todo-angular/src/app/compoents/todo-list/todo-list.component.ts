fc09fb326c827484675c68fd5e9a6e17b3da7154
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-07-04 17:53:23 +0200

    Add Todo cancel edit feature

--- todo-angular/src/app/compoents/todo-list/todo-list.component.ts
+++ todo-angular/src/app/compoents/todo-list/todo-list.component.ts
@@ -11,10 +11,12 @@ export class TodoListComponent implements OnInit {
   todos: Todo[];
   todoTitle: string;
   idForTodo: number;
+  beforeEditCache: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.beforeEditCache = '';
     this.idForTodo = 4;
     this.todoTitle = '';
     this.todos = [
@@ -62,10 +64,19 @@ export class TodoListComponent implements OnInit {
   }
 
   doneEdit(todo: Todo): void {
+    if (todo.title.trim().length === 0) {
+      todo.title = this.beforeEditCache;
+    }
+    todo.editing = false;
+  }
+
+  cancelEdit(todo: Todo): void {
+    todo.title = this.beforeEditCache;
     todo.editing = false;
   }
 
   editTodo(todo: Todo): void {
+    this.beforeEditCache = todo.title;
     todo.editing = true;
   }
 }
