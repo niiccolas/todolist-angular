9ff7b357f494b10e5fc93ba4308e95fd125e6ca1
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-07-04 16:36:13 +0200

    Add todos array to hold todos

--- todo-angular/src/app/compoents/todo-list/todo-list.component.ts
+++ todo-angular/src/app/compoents/todo-list/todo-list.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-todo-list',
+  selector: 'todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.sass']
 })
 export class TodoListComponent implements OnInit {
+  todos: object[];
 
   constructor() { }
 
   ngOnInit() {
+    this.todos = [
+      {
+        id: 1,
+        title: 'Go to bed early',
+        completed: false,
+        editing: false,
+      },
+      {
+        id: 2,
+        title: 'Learn Java',
+        completed: false,
+        editing: false,
+      },
+      {
+        id: 3,
+        title: 'Buy pizza',
+        completed: false,
+        editing: false,
+      },
+    ];
   }
 
 }
